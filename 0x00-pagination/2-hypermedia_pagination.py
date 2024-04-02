baddbc4c26eed877cd1e0dfa97c0ae8cfcbc5d01
willyopera <willy4opera@gmail.com>
2024-04-02 02:25:36 -0400

    Added the 2-hypermedia_pagination.py~

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -39,7 +39,7 @@ class Server:
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Fetches a page of data.
         """
-        assert type(page) == int and type(page_size) == int
+        assert isinstance(page, int) and isinstance(page_size, int)
         assert page > 0 and page_size > 0
         begin, last = index_range(page, page_size)
         data = self.dataset()
