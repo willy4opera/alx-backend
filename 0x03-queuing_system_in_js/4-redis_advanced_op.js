6ee158d060a2763cc57e6d9901566ac084c75bff
willyopera <willy4opera@gmail.com>
2024-05-08 20:30:18 -0400

    Added the 4-redis_advanced_op.js

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,39 @@
+#!/usr/bin/yarn dev
+
+// Node Redis client and advanced operations.
+
+import { createClient, print } from 'redis';
+
+const rsClient = createClient();
+
+rsClient.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
+
+const updateHash = (hashName, fieldName, fieldValue) => {
+  rsClient.HSET(hashName, fieldName, fieldValue, print);
+};
+
+const printHash = (hashName) => {
+  rsClient.HGETALL(hashName, (_err, reply) => console.log(reply));
+};
+
+function main() {
+  const hashObj = {
+    Portland: 50,
+    Seattle: 80,
+    'New York': 20,
+    Bogota: 20,
+    Cali: 40,
+    Paris: 2,
+  };
+  for (const [field, value] of Object.entries(hashObj)) {
+    updateHash('HolbertonSchools', field, value);
+  }
+  printHash('HolbertonSchools');
+}
+
+rsClient.on('connect', () => {
+  console.log('Redis client connected to the server');
+  main();
+});
