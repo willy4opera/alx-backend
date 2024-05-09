c27f195de7b2aa996f83568c4426d6180d0ba4a3
willyopera <willy4opera@gmail.com>
2024-05-08 20:59:46 -0400

    Adde the 6-job_creator.js

---
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,19 @@
+#!/usr/bin/yarn dev
+import { createQueue } from 'kue';
+
+// Create the Job Processor.
+
+const queue = createQueue();
+
+const sendNotification = (phoneNumber, message) => {
+  console.log(
+    `Sending notification to ${phoneNumber},`,
+    'with message:',
+    message,
+  );
+};
+
+queue.process('push_notification_code', (job, done) => {
+  sendNotification(job.data.phoneNumber, job.data.message);
+  done();
+});
